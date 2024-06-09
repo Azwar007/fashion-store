fa1349e00c71c0dead102f371d6f89c570e18bfb
malik <azwr007@gmail.com>
2024-06-10 02:59:42 +0500

    complete web

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../App.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>AZWAR Fashion Store &copy;2024</p>
+      <p>Contact us:+92 3153096677</p>
+      <p>
+        <a href="https://www.instagram.com/azwr_007/">
+        INSTAGRAM
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
